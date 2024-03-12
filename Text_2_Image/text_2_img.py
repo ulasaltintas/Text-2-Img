9b884cd64d7e0e93c935b9494cddf22eb5032a56
Ulas Altintas <145367204+ulasaltintas@users.noreply.github.com>
2024-03-12 16:11:56 +0300

    Add files via upload

---
+++ Text_2_Image/text_2_img.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from openai import OpenAI
+
+app = Flask(__name__, static_url_path='/static')
+client = OpenAI(api_key="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")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    prompt_text = request.form['text']
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt_text,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+    return render_template('result.html', image_url=image_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
